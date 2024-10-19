93f7f3658e4a4b88452b9a05052a80c57de8ab03
dxj711 <133747619+dxj711@users.noreply.github.com>
2024-10-19 13:17:19 +0530

    Create flask.py

---
+++ flask.py
@@ -0,0 +1,48 @@
+from flask import Flask
+import os
+import subprocess
+from datetime import datetime
+import pytz
+
+app = Flask(__name__)
+
+
+def get_top_output():
+    
+    process = subprocess.Popen(['top', '-b', '-n', '1'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    stdout, stderr = process.communicate()
+    if stderr:
+        return f"Error: {stderr.decode()}"
+    return stdout.decode()
+
+
+def get_ist_time():
+    ist = pytz.timezone('Asia/Kolkata')
+    return datetime.now(ist).strftime('%Y-%m-%d %H:%M:%S')
+
+@app.route('/htop')
+def htop():
+    
+    full_name = "Davis Joseph"  
+    system_username = os.getlogin()
+    ist_time = get_ist_time()
+    top_output = get_top_output()
+
+    
+    html = f"""
+    <html>
+    <head><title>System Information - /htop</title></head>
+    <body>
+        <h1>System Information</h1>
+        <p><b>Name:</b> {full_name}</p>
+        <p><b>Username:</b> {system_username}</p>
+        <p><b>Server Time (IST):</b> {ist_time}</p>
+        <h2>Top Output</h2>
+        <pre>{top_output}</pre>
+    </body>
+    </html>
+    """
+    return html
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080, debug=True)
